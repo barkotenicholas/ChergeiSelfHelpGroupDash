c1cd8a081aeaf384202b2b575a36f3d9eba8e3f8
barkotenicholas <barkotenicholas@gmail.com>
2022-12-09 09:39:57 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useRoutes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  const element = useRoutes([])
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
