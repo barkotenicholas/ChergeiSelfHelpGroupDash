71555a256b4def08ff286995112fb8a3cbc78d7b
barkotenicholas <barkotenicholas@gmail.com>
2023-01-05 08:51:32 +0300

    Refactored file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
-import { useRoutes } from 'react-router-dom'
-import './App.css'
-import Home from './components/Home/Home'
-import Login from './components/login/Login'
+import React from "react";
+import {AppRoutes} from "./routes";
 
-function App() {
+const App = () => {
+  return (
+    <>
+      <AppRoutes />
+    </>
+  );
+};
 
-  const element = useRoutes([
-    {
-      path:'/',
-      element : <Home/>
-    },
-    {
-      path:'/login',
-      element:<Login/>
-    }
-  ])
-
-  return element
-}
-
-export default App
+export default App;
