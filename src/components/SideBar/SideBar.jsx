d155b227956ab5a669fcd67c92d2fafd31f837f9
=barkotenicholas <=>
2023-01-18 17:51:53 +0300

    changed sidebar size

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -5,12 +5,12 @@ const SideBar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <div className={`${toggle ? 'w-[5.8rem]' : ''} sidebar-container`}>
-      <div className="absolute top-[7rem] flex justify-center items-center -left-5 w-10 h-10 bg-glass rounded-full cursor-pointer " onClick={()=>{
+      <div className='absolute top-[7rem] flex justify-center items-center -left-5 w-10 h-10 bg-glass rounded-full cursor-pointer' onClick={()=>{
         setToggle(!toggle);
       }}>
         <BiChevronLeft
           className={`${
-            toggle ? "rotate-180" : ""
+            toggle ? 'rotate-180' : ''
           } text-3xl transition-all duration-300 `}
         />
       </div>
